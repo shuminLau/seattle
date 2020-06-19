c0a3a7677299aed9607045f79b67722642d6609c
shuminlau <shuminlau@xiyanghui.com>
2020-06-19 18:28:04 +0800

    动画1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 timeline1();
 
 $(function () {
@@ -38,20 +37,43 @@ function timeline1() {
     timeLine.add(TweenLite.fromTo('#font1', 1, {}, {opacity: 0, delay: 1.5}))
     timeLine.add(TweenLite.fromTo('#font2', 1.5, {opacity: 0}, {opacity: 1}))
     timeLine.add(TweenLite.fromTo(['#font2', '#bg1'], 1, {}, {opacity: 0, delay: 1.5}),)
-    //
+    //frame2
     timeLine.add(TweenLite.fromTo("#logo", 0.5, {y: 100, opacity: 0}, {y: 0, opacity: 1}))
     timeLine.add(TweenLite.fromTo("#line", 1, {y: -100, opacity: 0}, {y: 0, opacity: 1}))
     timeLine.add(TweenLite.fromTo('#font3', 1, {opacity: 0}, {opacity: 1}))
     timeLine.add(TweenLite.fromTo('#font4', 1, {opacity: 0}, {opacity: 1}))
-    timeLine.play()
+    timeLine.play(1)
 }
 
 //
 function timeline2() {
+
     const timeLine = new TimelineMax()
-    // new TweenMax("#waterfall",2,{y:-500,repeat:-1})
-// new TweenMax("#waterfall1",2,{y:100,repeat:-1})
-// new TweenMax("#waterfall2",2,{y:-500,repeat:-1})
+    timeLine.add(TweenLite.fromTo( "#waterfall1", 8, {y: 0}, {y: -500,delay:3} ))
+    const timeLine1 = new TimelineMax()
+    timeLine1.add(TweenLite.fromTo( ["#waterfall","#waterfall2"] ,8, {y: -500}, {y: 0} ))
+    timeLine1.add(TweenLite.fromTo(["#waterfall1","#waterfall","#waterfall2","#font5","#font6",],3,{opacity:1},{opacity:0}))
+    //frame2
+    const brand = new TimelineMax({repeat:1,delay:11});
+    brand.to("#brand", 0.5, {text: {value: "sadfd", delimiter: " "}, ease: Linear.easeNone})
+        .to("#brand", 0.5, {text: {value: "sadsaa", delimiter: " "}, ease: Linear.easeNone})
+        .to("#brand", 0.5, {text: {value: "asasdd", delimiter: " "}, ease: Linear.easeNone})
+        .to("#brand", 0.5, {text: {value: "asasdd", delimiter: " "}, ease: Linear.easeNone})
+        .to("#brand", 0.5, {text: {value: "asdas", delimiter: " "}, ease: Linear.easeNone});
+    timeLine1.add(TweenLite.to('#brand',1,{opacity:1}))
+    const heart =new TimelineMax({repeat:10,delay:11});
+    heart.fromTo( "#heart", 0.5,{scale:1},{opacity: 1,scale: 1.1})
+
+
+
+
+    const timeLine2 = new TimelineMax()
+    timeLine2.add(TweenLite.fromTo('#font5', 2, {x:400}, {x: 0}))
+    timeLine2.add(TweenLite.fromTo('#font6', 2, {x:400}, {x: 0}))
+
+    const timeLine3 = new TimelineMax()
+    timeLine3.add(TweenLite.fromTo( ["#waterfall-box"], 1, {opacity: 0,}, {opacity: 1} ))
+
 
 }
 
@@ -59,18 +81,27 @@ function timeline3() {
     const timeLine = new TimelineMax()
 
 }
+
 function timeline4() {
     const timeLine = new TimelineMax()
 
 }
+
 function timeline5() {
     const timeLine = new TimelineMax()
 
 }
+
 function timeline6() {
     const timeLine = new TimelineMax()
 
 }
+
+
+
+
+
+
 
 
 // timeLine.add(TweenLite.from('#font5', 0.5, {left: 100, autoAlpha: 0}, "-=0.25"))
@@ -99,14 +130,9 @@ function timeline6() {
 // }, 0.05);
 //
 // //品牌
-// const tm = new TimelineMax({repeat:-1});
-// tm.to("#brand", 0.5, {text: {value: "sadfd", delimiter: " "}, ease: Linear.easeNone})
-//     .to("#brand", 0.5, {text: {value: "sadsaa", delimiter: " "}, ease: Linear.easeNone})
-//     .to("#brand", 0.5, {text: {value: "asasdd", delimiter: " "}, ease: Linear.easeNone})
-//     .to("#brand", 0.5, {text: {value: "asasdd", delimiter: " "}, ease: Linear.easeNone})
-//     .to("#brand", 0.5, {text: {value: "asdas", delimiter: " "}, ease: Linear.easeNone});
-//
-//
+
+
+
 
 
 //
